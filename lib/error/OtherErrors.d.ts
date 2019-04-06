c49aae3c1be2945cd21d9f758e12313d190e3d09
Giáo Hồ <loveme05ye@gmail.com>
2019-04-07 03:06:14 +0700

    Add error classes

---
+++ lib/error/OtherErrors.d.ts
@@ -0,0 +1,9 @@
+export declare class FormattedError extends Error {
+    constructor(message?: string);
+}
+export declare class OverridingError extends Error {
+    constructor(message?: string);
+}
+export declare class DuplicationError extends Error {
+    constructor(message?: string);
+}
